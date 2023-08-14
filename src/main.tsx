16bc1f443ab9c0e76a00e568c60c7a31a6c47dda
Alex Kim <alexgkim205@gmail.com>
2023-08-14 13:53:11 -0400

    feat: add posthog

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {resetContext} from "kea";
+import posthog from 'posthog-js'
 
 resetContext({
     plugins: [],
+})
+
+posthog.init('phc_xr7rXLgAonX1dt5xwANhxzla7a5Uj5gWMkfGuoj8eui', { api_host: 'https://app.posthog.com',
+    autocapture: false,
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
